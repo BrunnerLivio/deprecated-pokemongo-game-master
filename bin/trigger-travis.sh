619da7f967d70036aaacb90ffcaf422def401b5f
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-02-26 16:18:26 +0100

    Add travis trigger

---
+++ bin/trigger-travis.sh
@@ -0,0 +1,57 @@
+
+#!/bin/bash
+## brief: trigger a downstream travis build
+## see: travis API documentation
+
+# variables
+USER=$1
+REPO=$2
+TRAVIS_ACCESS_TOKEN=$3
+BRANCH=$4
+MESSAGE=$5
+
+# login to travis and get token
+travis login --skip-completion-check --github-token $TRAVIS_ACCESS_TOKEN
+travis whoami --skip-completion-check
+TOKEN=$(travis token --skip-completion-check)
+IFS=' ' read -r -a array <<< "$TOKEN"
+TOKEN=${array[${#array[@]}-1]}
+
+# inspired from plume-lib, check arguments and add message
+if [ $# -eq 5 ] ; then
+    MESSAGE=",\"message\": \"$5\""
+elif [ -n "$TRAVIS_REPO_SLUG" ] ; then
+    MESSAGE=",\"message\": \"Triggered from upstream build of $TRAVIS_REPO_SLUG by commit "`git rev-parse --short HEAD`"\""
+fi
+
+# for debugging
+echo "USER=$USER"
+echo "REPO=$REPO"
+echo "TOKEN: ${array[${#array[@]}-1]}"
+echo "BRANCH=$BRANCH"
+echo "MESSAGE=$MESSAGE"
+
+# curl POST request content body
+BODY="{
+  \"request\": {
+  \"branch\":\"$BRANCH\"
+  $MESSAGE
+}}"
+
+# make a POST request with curl (note %2F could be replaced with 
+# / and additional curl arguments, however this works too!)
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${TOKEN}" \
+  -d "$BODY" \
+  https://api.travis-ci.org/repo/${USER}%2F${REPO}/requests \
+  | tee /tmp/travis-request-output.$$.txt
+
+if grep -q '"@type": "error"' /tmp/travis-request-output.$$.txt; then
+   cat /tmp/travis-request-output.$$.txt
+   exit 1
+elif grep -q 'access denied' /tmp/travis-request-output.$$.txt; then
+   cat /tmp/travis-request-output.$$.txt
+   exit 1
---
+++ bin/trigger.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## brief: wrapper script which calls trigger-travis
+## If the branch name of the upstream and downstream project are equal and
+## we have a valid travis configuration in the downstream project then
+## try to trigger a travis build with the given github/travis user and
+## the given github/travis access token and log a certain message in the
+## travis downstream project build for tracing back the dependent builds
+
+# arguments
+USER=$1
+DOWNSTREAM_REPO=$2
+BRANCH=$3
+TRAVIS_ACCESS_TOKEN=$4
+MESSAGE=$5
+
+# fetch trigger-travis script and make executable
+curl -LO "https://raw.github.com/brunnerlivio/pokemongo-game-master/master/bin/trigger-travis.sh"
+chmod +x trigger-travis.sh
+
+# check for correct input
+#if [ $# -lt 4 ] ; then
+ #  usage()
+#fi
+
+# trigger build if above conditions hold
+#if [[ ($TRAVIS_BRANCH == $3) &&
+if [[ ($TRAVIS_PULL_REQUEST == false) &&
+   ( (! $TRAVIS_JOB_NUMBER == *.*) || ($TRAVIS_JOB_NUMBER == *.1) ) ]] ; then
+   chmod +x trigger-travis.sh
+   ./trigger-travis.sh $1 $2 $4 $3 $5
+fi
+
+# usage function
+function usage {
+   echo "$(basename $0): USER DOWNSTREAM_REPOSITORY BRANCH TRAVIS_ACCESS_TOKEN"
+}
